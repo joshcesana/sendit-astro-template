d4a56546fee7a9459b374951b4b9ec42c0de68d0
joshcesana <joshuacesana@gmail.com>
2024-01-16 10:57:33 -0500

    Fix site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import bookshop from '@bookshop/astro-bookshop';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://top-quail.cloudvent.net/",
+  site: "https://fun-stork.cloudvent.net/",
   integrations: [bookshop(), react()]
 });
